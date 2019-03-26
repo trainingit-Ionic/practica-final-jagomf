9506614bda6a533e2074890372233bffff81b98e
Jago <jago84@gmail.com>
2019-03-26 10:48:10 +0100

    Quitado módulo home, no necesario para esta app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    CommonModule,
+    FormsModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
